0f5b2afde601923dd84063ab2437f72b7428655d
sani816 <saniakhatun622@gmail.com>
2026-06-12 16:25:44 +0530

    booking

---
+++ backend/models/Booking.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    company: String,
+    budget: String,
+    message: String,
+
+    type: {
+      type: String,
+      enum: ["contact", "consultation"],
+      default: "contact",
+    },
+
+
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Booking", bookingSchema);
