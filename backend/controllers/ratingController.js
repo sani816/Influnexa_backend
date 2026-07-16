fd91ab79b2ce0659536e2c68e98cf919b8bbde1f
sani816 <saniakhatun622@gmail.com>
2026-07-16 10:29:16 +0530

    update

--- backend/controllers/ratingController.js
+++ backend/controllers/ratingController.js
@@ -1,58 +1,58 @@
-import Rating from "../models/Rating.js";
+// import Rating from "../models/Rating.js";
 
-export const createRating = async (
-  req,
-  res
-) => {
-  try {
-    const rating = await Rating.create(
-      req.body
-    );
+// export const createRating = async (
+//   req,
+//   res
+// ) => {
+//   try {
+//     const rating = await Rating.create(
+//       req.body
+//     );
 
-    res.status(201).json(rating);
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-};
+//     res.status(201).json(rating);
+//   } catch (error) {
+//     res.status(500).json({
+//       message: error.message,
+//     });
+//   }
+// };
 
-export const getRatings = async (
-  req,
-  res
-) => {
-  try {
-    const ratings = await Rating.find();
+// export const getRatings = async (
+//   req,
+//   res
+// ) => {
+//   try {
+//     const ratings = await Rating.find();
 
-    res.json(ratings);
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-};
+//     res.json(ratings);
+//   } catch (error) {
+//     res.status(500).json({
+//       message: error.message,
+//     });
+//   }
+// };
 
-export const getAverageRating = async (
-  req,
-  res
-) => {
-  try {
-    const ratings = await Rating.find();
+// export const getAverageRating = async (
+//   req,
+//   res
+// ) => {
+//   try {
+//     const ratings = await Rating.find();
 
-    const average =
-      ratings.reduce(
-        (sum, item) => sum + item.rating,
-        0
-      ) / (ratings.length || 1);
+//     const average =
+//       ratings.reduce(
+//         (sum, item) => sum + item.rating,
+//         0
+//       ) / (ratings.length || 1);
 
-    res.json({
-      totalRatings: ratings.length,
-      averageRating:
-        average.toFixed(1),
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-};
+//     res.json({
+//       totalRatings: ratings.length,
+//       averageRating:
+//         average.toFixed(1),
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       message: error.message,
+//     });
+//   }
+// };
