5836da0f76c13d4f12371ec771f3736ccd4f29a4
sani816 <saniakhatun622@gmail.com>
2026-07-16 10:30:14 +0530

    update

--- backend/routes/ratingRoutes.js
+++ backend/routes/ratingRoutes.js
@@ -1,20 +1,20 @@
-import express from "express";
+// import express from "express";
 
-import {
-  createRating,
-  getRatings,
-  getAverageRating,
-} from "../controllers/ratingController.js";
+// import {
+//   createRating,
+//   getRatings,
+//   getAverageRating,
+// } from "../controllers/ratingController.js";
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post("/", createRating);
+// router.post("/", createRating);
 
-router.get("/", getRatings);
+// router.get("/", getRatings);
 
-router.get(
-  "/average",
-  getAverageRating
-);
+// router.get(
+//   "/average",
+//   getAverageRating
+// );
 
-export default router;
+// export default router;
