3f48d5d557e39839c4477a2e9efa1879194a0ceb
sani816 <saniakhatun622@gmail.com>
2026-07-20 13:55:22 +0530

    update

--- backend/controllers/consultationController.js
+++ backend/controllers/consultationController.js
@@ -16,15 +16,15 @@ export const bookConsultation = async (req, res) => {
         message,
       });
 
-    const transporter =
-      nodemailer.createTransport({
-        service: "gmail",
+    // const transporter =
+    //   nodemailer.createTransport({
+    //     service: "gmail",
 
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
-      });
+    //     auth: {
+    //       user: process.env.EMAIL_USER,
+    //       pass: process.env.EMAIL_PASS,
+    //     },
+    //   });
 
     // await transporter.sendMail({
     //   from: process.env.EMAIL_USER,
