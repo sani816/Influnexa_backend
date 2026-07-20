6c90ac74034a19bc4cb967e285ffb7bce2fbbcf6
sani816 <saniakhatun622@gmail.com>
2026-07-20 10:56:13 +0530

    up

--- backend/server.js
+++ backend/server.js
@@ -77,7 +77,7 @@ app.use("/uploads", express.static("uploads"));
 // =======================
 // 🔥 START SERVER
 // =======================
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
