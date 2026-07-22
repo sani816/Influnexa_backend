3be40a011c9d92f0ba183ade567aee1bba80a477
sani816 <saniakhatun622@gmail.com>
2026-07-22 12:30:27 +0530

    .

--- backend/middleware/uploadBlog.js
+++ backend/middleware/uploadBlog.js
@@ -3,7 +3,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "uploads/blogs");
+    cb(null, "uploads");
   },
 
   filename(req, file, cb) {
